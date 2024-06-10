3c46c067e33eb2683a7d873c02e10a5d59fb7353
ekanir52 <anir@sijo.fr>
2024-06-10 13:56:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from library_functions import is_image, is_pdf, convert_pdf_to_image, pp_image, pp_image2, ocr_extract_text, calculate_cer, calculate_wer
-import os, shutil
+from library_functions import fclear, is_image, is_pdf, convert_pdf_to_image, pp_image, ocr_extract_text, calculate_cer, calculate_wer
+# import os, shutil
 
 
 def __main__():
+    
+    fclear()
+
+    alpha = 1.2
+    beta = 15
+    dil_rect = (7,6)
+    dil_iterations = 25
 
     file = "./pdf_cv/cv_1.pdf"
 
@@ -20,20 +27,22 @@ def __main__():
         return False
 
     print(" Preprocessing the Resume image ...", file.__str__())
-    # image = pp_image(file)
-    image = pp_image2(file)
-    print(" Extracting the data from resume ... ")
-    ocr_text = ocr_extract_text(image)
+    image = pp_image(file, alpha=alpha, beta=beta, dil_rect_x_y=dil_rect, dil_n_iteration=dil_iterations)
 
-    print(" Calculating the error rates ....")
-    cer = calculate_cer(ocr_text, ground_truth_text)
-    wer = calculate_wer(ocr_text, ground_truth_text)
+    # print(image)
+    # print(" Extracting the data from resume ... ")
+    
+    # # Tesseract custom configuration : r'--oem 3 --psm 6' (psm = 4 best one so far)
+    # custom_config = r'--oem 3 --psm 4'
+    # ocr_text = ocr_extract_text(image, custom_config)
+
+    # print(" Calculating the error rates ....")
+    # cer = calculate_cer(ocr_text, ground_truth_text)
+    # wer = calculate_wer(ocr_text, ground_truth_text)
 
 
-    print(f"CER : {cer}")
-    print(f"WER : {wer}")
-
-
+    # print(f"CER : {cer}")
+    # print(f"WER : {wer}")
 
     return True
     
